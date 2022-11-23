ce3c802191731f7f27a87e270dce910b8db13e55
Ian Heraty <ian.heraty@pm.me>
2022-11-23 17:43:36 -0600

    Add max width to image in movie index.

--- client/src/pages/MoviePage.tsx
+++ client/src/pages/MoviePage.tsx
@@ -64,6 +64,7 @@ export default class MoviePage extends React.Component<{}, S> {
               <h5>{title}</h5>
               <img
                 src={posterUrl || "//www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"}
+                style={{ maxWidth: 200 }}
                 alt="movie poster"
               />
             </div>
